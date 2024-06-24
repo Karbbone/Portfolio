7f9d96f9e2583f69b9fe5ee2c46a25dc97936b8d
Karbbone <clement.maillet56@gmail.com>
2024-06-24 14:58:07 +0200

    feat: Update SchoolSection component with improved descriptions and logos and ExpSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
+import React from "react";
 import { Header } from "@/_components/Header";
 import { Hero } from "@/_components/Hero";
-import { Section } from "@/_components/Section";
 import { TechSection } from "@/_components/TechSection";
-import Image from "next/image";
-import iutlogo from "../_components/iutvannes.png";
-import mediamaglogo from "../_components/mediamag.png";
-import { Card } from "@/components/ui/card";
-import React from "react";
 import { SchoolSection } from "@/_components/SchoolSection";
+import { ExpSection } from "@/_components/ExpSection";
 
 export default function Home() {
   return (
@@ -15,36 +11,8 @@ export default function Home() {
       <Header />
       <Hero />
       <TechSection />
+      <ExpSection />
       <SchoolSection />
-      <Section className="my-9 flex flex-col items-center sm:flex-row sm:items-start bg-background rounded border">
-        <div className="flex-[2]">
-          <h1 className="text-2xl font-caption font-bold text-center">
-            POSTE/ECOLE
-          </h1>
-          <div className="flex flex-col gap-4 px-4">
-            <Card className="w-full p-2">
-              <div className="flex flex-row items-center justify-center gap-2">
-                <Image
-                  src={mediamaglogo}
-                  alt="mediamag-logo"
-                  className="w-20 h-auto"
-                />
-                <p>
-                  Mediamag Système <br /> 2023 - Maintenant
-                </p>
-              </div>
-            </Card>
-            <Card className="w-full">
-              <div className="flex flex-row items-center justify-center gap-2">
-                <Image src={iutlogo} alt="iut-logo" className="w-20 h-auto" />
-                <p>
-                  IUT DE VANNES <br /> 2020-2024
-                </p>
-              </div>
-            </Card>
-          </div>
-        </div>
-      </Section>
     </main>
   );
 }
