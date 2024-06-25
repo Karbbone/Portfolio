9d648deabbdb89ffb840f1035fac9e1394bd1cf5
Karbbone56 <clement.maillet56@gmail.com>
2024-06-25 22:36:21 +0200

    refactor: Update SkillSection component refacto

--- src/_components/sections/SkillSection.tsx
+++ src/_components/sections/SkillSection.tsx
@@ -5,6 +5,28 @@ import { GlassIcon } from "../svg_icons/GlassIcon";
 import { TeamIcon } from "../svg_icons/TeamIcon";
 import { PuzzleIcon } from "../svg_icons/PuzzleIcon";
 
+const skills = [
+  {
+    title: "Travail d'équipe",
+    description:
+      "J'apprécie grandement le travail d'équipe et le partage de mes connaissances pour favoriser une croissance collective.",
+    Icon: TeamIcon,
+  },
+  {
+    title: "Versatile",
+    description:
+      "Je suis pleinement capable de m'adapter rapidement à de nouvelles technologies et à des environnements variés.",
+    Icon: PuzzleIcon,
+  },
+  {
+    title: "Curieux",
+    description:
+      "Ma curiosité me pousse à explorer et à maîtriser de nouvelles technologies, enrichissant ainsi mes connaissances chaque jour. Cette soif d'apprendre stimule mon développement professionnel continu et renforce ma capacité.",
+    Icon: GlassIcon,
+    colSpan: 2,
+  },
+];
+
 export const SkillSection = () => {
   return (
     <Section className="my-9 flex items-center sm:items-start bg-background rounded border">
@@ -13,53 +35,23 @@ export const SkillSection = () => {
           {"Mes compétences"}
         </h2>
         <div className="justify-center py-4 flex flex-col gap-8">
-          <div className="grid md:grid-cols-2  grid-cols-1 justify-center gap-2">
-            <Card>
-              <CardHeader>
-                <div className="flex justify-between items-center">
-                  <CardTitle>Travail d&apos;équipe</CardTitle>
-                  <TeamIcon size={40} color="white" />
-                </div>
-              </CardHeader>
-              <CardContent>
-                <p>
-                  J&apos;apprécie grandement le travail d&apos;équipe et le
-                  partage de mes connaissances pour favoriser une croissance
-                  collective.
-                </p>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader>
-                <div className="flex justify-between items-center">
-                  <CardTitle>Versatile</CardTitle>
-                  <PuzzleIcon size={40} color="white" />
-                </div>
-              </CardHeader>
-              <CardContent>
-                <p>
-                  Je suis pleinement capable de m&apos;adapter rapidement à de
-                  nouvelles technologies et à des environnements variés.
-                </p>
-              </CardContent>
-            </Card>
-            <Card className="card md:col-span-2">
-              <CardHeader>
-                <div className="flex justify-between items-center">
-                  <CardTitle>Curieux</CardTitle>
-                  <GlassIcon size={40} color="white" />
-                </div>
-              </CardHeader>
-              <CardContent>
-                <p>
-                  Ma curiosité naturelle me pousse à explorer et à maîtriser de
-                  nouvelles technologies, enrichissant ainsi mes connaissances
-                  chaque jour. Cette soif d&apos;apprendre stimule mon
-                  développement professionnel continu et renforce ma capacité à
-                  innover.
-                </p>
-              </CardContent>
-            </Card>
+          <div className="grid md:grid-cols-2 grid-cols-1 justify-center gap-2">
+            {skills.map(({ title, description, Icon, colSpan }) => (
+              <Card
+                key={title}
+                className={`card ${colSpan === 2 ? "md:col-span-2" : ""}`}
+              >
+                <CardHeader>
+                  <div className="flex justify-between items-center">
+                    <CardTitle>{title}</CardTitle>
+                    <Icon size={40} color="white" />
+                  </div>
+                </CardHeader>
+                <CardContent>
+                  <p>{description}</p>
+                </CardContent>
+              </Card>
+            ))}
           </div>
         </div>
       </div>
