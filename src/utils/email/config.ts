963fddf9936d1c7e9990e2693fabeba44799f93b
Karbbone <clement.maillet56@gmail.com>
2026-02-10 22:36:43 +0100

    feat : logs about smtp

--- src/utils/email/config.ts
+++ src/utils/email/config.ts
@@ -1,14 +1,16 @@
 import type { EmailConfig } from './types';
 
 export function getEmailConfig(): EmailConfig {
+  console.log('SMTP_HOST:', process.env.SMTP_HOST);
+  console.log('SMTP_PORT:', process.env.SMTP_PORT);
   return {
-    host: import.meta.env.SMTP_HOST,
-    port: Number.parseInt(import.meta.env.SMTP_PORT || '587'),
-    secure: import.meta.env.SMTP_SECURE === 'true',
-    user: import.meta.env.SMTP_USER,
-    pass: import.meta.env.SMTP_PASS,
-    from: import.meta.env.SMTP_FROM,
-    to: import.meta.env.SMTP_TO,
+    host: process.env.SMTP_HOST ?? '',
+    port: Number.parseInt(process.env.SMTP_PORT || '587'),
+    secure: process.env.SMTP_SECURE === 'true',
+    user: process.env.SMTP_USER ?? '',
+    pass: process.env.SMTP_PASS ?? '',
+    from: process.env.SMTP_FROM ?? '',
+    to: process.env.SMTP_TO ?? '',
   };
 }
 
